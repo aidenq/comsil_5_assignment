5b18ccd8a2b6cef8a356d58528b51b011ca3d92c
MANGDUB <aidenq@naver.com>
2024-04-09 18:02:30 +0900

    master

---
+++ calculator.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int fn, sn;
+    char op;
+
+    printf("first num: ");
+    scanf("%d", &fn);
+
+    printf("second num: ");
+    scanf("%d", &sn);
+
+    printf("operator: ");
+    scanf(" %c", &op);
+
+    if (op == '+')
+        printf("%d", fn+sn);
+    else
+        printf("Invalid Operator");
+
+    return 0;
+}
