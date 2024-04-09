af180409f2e521ec1aab1e50b3bfc42f38ff0f3a
MANGDUB <aidenq@naver.com>
2024-04-09 18:59:50 +0900

    branch2

--- calculator.c
+++ calculator.c
@@ -16,9 +16,11 @@ int main() {
     if (op == '+')
         printf("%d", fn + sn);
     else if (op == '-')
-        printf("%d", fn * sn);
+        printf("%d", fn - sn);
     else if (op == '*')
         printf("%d", fn * sn);
+    else if (op == '/')
+        printf("%d", fn / sn);
     else
         printf("Invalid Operator");
 
